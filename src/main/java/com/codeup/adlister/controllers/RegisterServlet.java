8ec9f8e01c4223d8a26bde2f136180b0aa3196ca
armandosegura <segura_arm@yahoo.com>
2018-04-06 10:26:43 -0500

    waiting on passwords walkthrough

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -22,40 +22,62 @@ public class RegisterServlet extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
-//         sets the checks for register form / fix, not quite working yet
-//        HttpSession session = request.getSession();
-//
-//        if(password == null || password.trim() == "") {
-//            session.removeAttribute("password_error");
-//            session.removeAttribute("email_error");
-//            session.setAttribute("username_error", "<p style=\"color:red\">Sorry \"username\" error!</p>");
-//            response.sendRedirect("/register");
-//        }else if (!password.equals(passwordConfirmation)) {
-//            session.removeAttribute("username_error");
-//            session.removeAttribute("email_error");
-//            session.setAttribute("password_error", "<p style=\"color:red\">Sorry \"password\" error!</p>");
-//            response.sendRedirect("/register");
-//        }else if (email == null || email == "") {
-//            session.removeAttribute("username_error");
-//            session.removeAttribute("password_error");
-//            session.setAttribute("email_error", "<p style=\"color:red\">Sorry \"email\" error!</p>");
-//            response.sendRedirect("/register");
-//        }
 
-        // validate input
-        boolean inputHasErrors = username.isEmpty()
-            || email.isEmpty()
-            || password.isEmpty()
-            || (! password.equals(passwordConfirmation));
+//         sets the checks for register form / add more features
+        HttpSession session = request.getSession();
 
-        if (inputHasErrors) {
+        if (password == null || password.trim() == "") {
+            session.removeAttribute("password_error");
+            session.removeAttribute("email_error");
+            session.removeAttribute("username_error");
+            session.removeAttribute("password_mismatch");
+            session.setAttribute("password_error", "<p style=\"color:red\">Sorry \"password\" error!</p>");
             response.sendRedirect("/register");
-            return;
-        }
+        } else if (!password.equals(passwordConfirmation)) {
+            session.removeAttribute("password_error");
+            session.removeAttribute("email_error");
+            session.removeAttribute("username_error");
+            session.removeAttribute("password_mismatch");
+            session.setAttribute("password_mismatch", "<p style=\"color:red\">Sorry \"passwords\" do not match!</p>");
+            response.sendRedirect("/register");
+        } else if (email == null || email.trim() == "") {
+            session.removeAttribute("password_error");
+            session.removeAttribute("email_error");
+            session.removeAttribute("username_error");
+            session.removeAttribute("password_mismatch");
+            session.setAttribute("email_error", "<p style=\"color:red\">Sorry \"email\" error!</p>");
+            response.sendRedirect("/register");
+        } else if (username == null || username.trim() == "") {
+            session.removeAttribute("password_error");
+            session.removeAttribute("email_error");
+            session.removeAttribute("username_error");
+            session.removeAttribute("password_mismatch");
+            session.setAttribute("username_error", "<p style=\"color:red\">Sorry \"username\" error!</p>");
+            response.sendRedirect("/register");
+        } else {
+            session.removeAttribute("password_error");
+            session.removeAttribute("password_mismatch");
+            session.removeAttribute("email_error");
+            session.removeAttribute("username_error");
+            session.setAttribute("username", username);
+            session.setAttribute("password", password);
+            session.setAttribute("email", email);
 
-        // create and save a new user
-        User user = new User(username, email, password);
-        DaoFactory.getUsersDao().insert(user);
-        response.sendRedirect("/login");
+            // validate input
+            boolean inputHasErrors = username.isEmpty()
+                    || email.isEmpty()
+                    || password.isEmpty()
+                    || (!password.equals(passwordConfirmation));
+
+            if (inputHasErrors) {
+                response.sendRedirect("/register");
+                return;
+            }
+
+            // create and save a new user
+            User user = new User(username, email, password);
+            DaoFactory.getUsersDao().insert(user);
+            response.sendRedirect("/login");
+        }
     }
 }
